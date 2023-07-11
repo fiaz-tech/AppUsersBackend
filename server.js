ebdf119dc1d4a341168b2d8e3fd9edf382471adb
Abiola Smith <saratsmith968@gmail.com>
2023-07-11 18:03:19 +0100

    cors added

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors'
 import connectDB from './config/db.js'
 const app = express();
 
@@ -18,6 +19,7 @@ app.use('/api/appUsers', appUserRoutes)
 
 app.use(notFound)
 app.use(errorHandler)
+app.use(cors())
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
